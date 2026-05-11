050fce6ff1b9cc7ecb67863af0d016ae99e7804e
kshitiz_vohra <kshitizvohra37@gmail.com>
2026-05-11 18:03:51 +0530

    new

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -20,7 +20,7 @@ app.register_blueprint(analytics_bp)
 @app.route('/')
 def home():
     return render_template('login.html')
-@app.route('/register')
+@app.route('/register' , methods=['GET'])
 def register():
     return render_template('register.html')
 @app.route('/dashboard')
